ad4234ea3b22017f03a82e8639b0aa2659a292d7
Ingo Bauersachs <ingo@jitsi.org>
2013-01-07 18:06:11 +0100

    Documentation

--- OpenCymd.Nps.SamplePlugin/SampleNpsPlugin.cs
+++ OpenCymd.Nps.SamplePlugin/SampleNpsPlugin.cs
@@ -6,36 +6,43 @@
     using log4net;
 
     using OpenCymd.Nps.Plugin;
-    using OpenCymd.Nps.Plugin.Native;
 
+    /// <summary>
+    /// Sample NPS plugin that prints all request attributes and the attributes of the accept response.
+    /// </summary>
     public class SampleNpsPlugin : INpsPlugin
     {
         private static readonly ILog Logger = LogManager.GetLogger(typeof(SampleNpsPlugin));
 
+        /// <inheritdoc/>
         public void Initialize()
         {
             Logger.Info("Initialize");
         }
 
+        /// <inheritdoc/>
         public void RadiusExtensionProcess(IExtensionControl control)
         {
             Logger.Info("RadiusExtensionProcess");
-            if (Logger.IsDebugEnabled)
+            Logger.DebugFormat("RequestType: {0}", control.RequestType);
+            Logger.DebugFormat("ResponseType: {0}", control.ResponseType);
+            Logger.DebugFormat("ExtensionPoint: {0}", control.ExtensionPoint);
+            foreach (var attrib in control.Request)
             {
-                Logger.DebugFormat("RequestType: {0}", control.RequestType);
-                Logger.DebugFormat("ResponseType: {0}", control.ResponseType);
-                Logger.DebugFormat("ExtensionPoint: {0}", control.ExtensionPoint);
-                if (control.ResponseType == RadiusCode.AccessAccept && control.ExtensionPoint == RadiusExtensionPoint.Authorization)
-                {
-                    foreach (var attrib in control.Request)
-                    {
-                        var attribName = Enum.IsDefined(typeof(RadiusAttributeType), attrib.AttributeId)
-                                             ? ((RadiusAttributeType)attrib.AttributeId).ToString()
-                                             : attrib.AttributeId.ToString(CultureInfo.InvariantCulture);
-                        var attribValue = attrib.Value is byte[] ? "byte[" + ((byte[])attrib.Value).Length + "]" : attrib.Value;
-                        Logger.DebugFormat("{0}={1}", attribName, attribValue);
-                    }
-                }
+                var attribName = Enum.IsDefined(typeof(RadiusAttributeType), attrib.AttributeId)
+                                        ? ((RadiusAttributeType)attrib.AttributeId).ToString()
+                                        : attrib.AttributeId.ToString(CultureInfo.InvariantCulture);
+                var attribValue = attrib.Value is byte[] ? "byte[" + ((byte[])attrib.Value).Length + "]" : attrib.Value;
+                Logger.DebugFormat("{0}={1}", attribName, attribValue);
+            }
+
+            foreach (var attrib in control.Response[RadiusCode.AccessAccept])
+            {
+                var attribName = Enum.IsDefined(typeof(RadiusAttributeType), attrib.AttributeId)
+                                     ? ((RadiusAttributeType)attrib.AttributeId).ToString()
+                                     : attrib.AttributeId.ToString(CultureInfo.InvariantCulture);
+                var attribValue = attrib.Value is byte[] ? "byte[" + ((byte[])attrib.Value).Length + "]" : attrib.Value;
+                Logger.DebugFormat("{0}={1}", attribName, attribValue);
             }
         }
     }
